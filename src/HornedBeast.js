54d8ed7dc5667e90b97c4547e2c10b31960aa48c
Ryan <superryanman1@gmail.com>
2022-03-15 17:03:13 -0700

    finished lab-2

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 class HornedBeast extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      likes: 0,
+    }
+  }
+  handleLikes = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  };
+  
   render(){
     return(
       <>
+      <article>
         <h2>{this.props.title}</h2>
-        <img src={this.props.image_url} alt={this.props.description} title={this.props.title}></img>
+        <p>Total 👺 Given:{this.state.likes} </p>
+        <img 
+        onClick={this.handleLikes}
+        src={this.props.image_url} 
+        alt={this.props.description} 
+        title={this.props.title}>
+        </img>
         <p>{this.props.description}</p>
+      </article>
       </>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 
+
 class Main extends React.Component{
   render(){
+    let beasts = []
+    this.props.data.forEach ((hornedBeast, index) => {
+      beasts.push(
+        <HornedBeast
+        image_url = {hornedBeast.image_url}
+        title = {hornedBeast.title}
+        description = {hornedBeast.description}
+        keyword = {hornedBeast.keyword}
+        horns = {hornedBeast.horns}
+        key={index}
+        >
+
+        </HornedBeast>
+      )
+    });
     return(
       <main>
-        <HornedBeast
-        image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-        title = "UniWhal"
-        description = "A unicorn and a narwhal nuzzling their horns"
-        />
-        <HornedBeast
-        image_url = "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-        title = "Rhino Family"
-        description = "Parent rhino with two babies"
-        />
+        {beasts}
       </main>
     )
   }
